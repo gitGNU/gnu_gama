31dcc3ab6ea189d723a8f92b70fe354d066bdff4
cepek <cepek>
2002-04-10 16:19:59 +0000

    *** empty log message ***

--- gmatvec/transvec.h
+++ gmatvec/transvec.h
@@ -1,5 +1,5 @@
 /*  
-    C++ Matrix/Vector templates (GNU GaMa / gMatVec 0.9.17)
+    C++ Matrix/Vector templates (GNU GaMa / gMatVec 0.9.18)
     Copyright (C) 1999  Ales Cepek <cepek@fsv.cvut.cz>
 
     This file is part of the gMatVec C++ Matrix/Vector template library.
@@ -20,7 +20,7 @@
 */
 
 /*
- *  $Id: transvec.h,v 1.4 2002/03/19 17:04:31 cepek Exp $
+ *  $Id: transvec.h,v 1.5 2002/04/10 16:19:59 cepek Exp $
  *  http://www.gnu.org/software/gama/
  */
 
