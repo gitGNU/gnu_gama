2483c67b49308c1b50253e2993ccdd94354effe1
cepek <cepek>
2002-10-24 17:09:11 +0000

    *** empty log message ***

---
+++ gamaprog/linux/gama-local/gama-local.cpp
@@ -0,0 +1,31 @@
+/*  
+    GNU GaMa / C++ GaMaLib Library
+    Copyright (C) 1999, 2002  Ales Cepek <cepek@fsv.cvut.cz>
+
+    This file is part of the GNU GaMa / C++ GaMaLib library.
+    
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+ *  $Id: gama-local.cpp,v 1.1 2002/10/24 17:12:37 cepek Exp $
+ */
+
+#include "gama-local-main.h"
+
+int main(int argc, char* argv[])
+{
+  return GaMa_Main(argc, argv);
+}
