aff7ae83a7c9c22a3df5f6dfe6f1989e9e5e816e
cepek <cepek>
2003-02-20 18:32:45 +0000

    *** empty log message ***

---
+++ gnu_gama/g3/parameter.h
@@ -0,0 +1,83 @@
+/*  
+   GNU Gama -- adjustment of geodetic networks
+   Copyright (C) 2003  Ales Cepek <cepek@fsv.cvut.cz>
+
+   This file is part of the GNU Gama library.
+   
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this library; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/* $Id: parameter.h,v 1.1 2003/02/20 18:35:01 cepek Exp $  */
+
+#include <cstddef>
+
+#ifndef GNU_gama_____g3______parameter______h__________GNUgamag3parameterh
+#define GNU_gama_____g3______parameter______h__________GNUgamag3parameterh
+
+
+namespace GNU_gama { namespace g3 {
+
+  using std::size_t;
+  class Parameter;
+
+
+  class ParameterList {
+  public:
+
+    ParameterList() : begin_(0), end_(0) {}
+    ~ParameterList();
+
+    Parameter* begin() const { return begin_; }
+    Parameter* end  () const { return end_;   }
+
+  protected:
+
+    Parameter* begin_;
+    Parameter* end_;
+    
+  };
+
+
+  class Parameter : public ParameterList {
+  public:
+    
+    Parameter() : cor(0) {}
+    virtual ~Parameter() {}
+    
+    double value     () const { return val + cor; }
+    double init_value() const { return val; }
+    double correction() const { return cor; }
+    size_t index     () const { return ind; }
+    bool   is_const  () const { return isc; }
+
+    void set_init_value(double p) { val = p; cor = 0; }
+    void set_correction(double p) { cor = p; }
+    void set_index     (size_t t) { ind = t; }
+    void set_is_const  (bool   b) { isc = b; } 
+    
+  private:
+    
+    double val;
+    double cor;
+    size_t ind;
+    bool   isc;
+
+  };
+
+  inline ParameterList::~ParameterList() { delete[] begin_; }
+  
+}}
+
+#endif
