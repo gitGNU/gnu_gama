50eec36b436d9f2cc263f3e9087c1bbe6a81d0a1
cepek <cepek>
2003-03-11 20:53:07 +0000

    *** empty log message ***

--- gnu_gama/pointbase.h
+++ gnu_gama/pointbase.h
@@ -20,10 +20,11 @@
 */
 
 /*
- *  $Id: pointbase.h,v 1.3 2003/03/08 20:39:31 cepek Exp $
+ *  $Id: pointbase.h,v 1.4 2003/03/11 20:53:07 cepek Exp $
  */
 
 #include <map>
+#include <iterator>
 
 
 #ifndef GNU_gama__point_data_h_gnugamapointbase___gnu_gama_pointbase
@@ -38,13 +39,12 @@ namespace GNU_gama {
     {
     private:
 
-      typedef std::map<typename Point::Name, Point*>  Points;
-      Points  points;
+      typedef  std::map<typename Point::Name, Point*>  Points;
+      Points   points;
+      typename Point::Common* common;
 
     public:    
 
-      typename Point::Common* common;
-      
       PointBase() : common(0) {}
       PointBase(const PointBase& cod);
       ~PointBase();
@@ -60,87 +60,93 @@ namespace GNU_gama {
       void erase(const typename Point::Name&);
       void erase();
       
-      class const_iterator {
-      public:
-
-        const_iterator(const typename Points::const_iterator& p) : pit(p) 
-          {
-          }
-        bool operator==(const const_iterator& x) const 
-          { 
-            return pit==x.pit; 
-          }
-        bool operator!=(const const_iterator& x) const 
-          { 
-            return pit!=x.pit; 
-          }
-        const_iterator& operator++()
-          {
-            ++pit;
-            return *this;
-          }
-        const_iterator operator++(int)
-          {
-            const_iterator tmp(pit);
-            ++pit;
-            return tmp;
-          }
-        const Point* operator*() const
-          {
-            return (*pit).second;
-          }
-
-      private:
-        typename Points::const_iterator pit;
-
-      };
-
+      class const_iterator
+        // : public std::iterator <std::forward_iterator_tag, Point> 
+        {
+        public:
+          
+          const_iterator(const typename Points::const_iterator& p) : pit(p) 
+            {
+            }
+          bool operator==(const const_iterator& x) const 
+            { 
+              return pit==x.pit; 
+            }
+          bool operator!=(const const_iterator& x) const 
+            { 
+              return pit!=x.pit; 
+            }
+          const_iterator& operator++()
+            {
+              ++pit;
+              return *this;
+            }
+          const_iterator operator++(int)
+            {
+              const_iterator tmp(pit);
+              ++pit;
+              return tmp;
+            }
+          const Point* operator*() const
+            {
+              return (*pit).second;
+            }
+          
+        private:
+          typename Points::const_iterator pit;
+          
+        };
+      
       const_iterator  begin() const { return points.begin(); }
       const_iterator  end  () const { return points.end  (); }
 
 
-      class iterator {
-      public:
+      class iterator 
+        // : public std::iterator <std::forward_iterator_tag, Point> 
+        {
+        public:
 
-        iterator(const typename Points::iterator& p) : pit(p) 
-          {
-          }
-        operator const_iterator() const
-          {
-            return const_iterator(pit);
-          }
-        bool operator==(const iterator& x) const 
-          { 
-            return pit==x.pit; 
-          }
-        bool operator!=(const iterator& x) const 
-          { 
-            return pit!=x.pit; 
-          }
-        iterator& operator++()
-          {
-            ++pit;
-            return *this;
-          }
-        iterator operator++(int)
-          {
-            iterator tmp(pit);
-            ++pit;
-            return tmp;
-          }
-        Point* operator*() const
-          {
-            return (*pit).second;
-          }
-
-      private:
-        typename Points::iterator pit;
-
-      };
-
+          iterator(const typename Points::iterator& p) : pit(p) 
+            {
+            }
+          operator const_iterator() const
+            {
+              return const_iterator(pit);
+            }
+          bool operator==(const iterator& x) const 
+            { 
+              return pit==x.pit; 
+            }
+          bool operator!=(const iterator& x) const 
+            { 
+              return pit!=x.pit; 
+            }
+          iterator& operator++()
+            {
+              ++pit;
+              return *this;
+            }
+          iterator operator++(int)
+            {
+              iterator tmp(pit);
+              ++pit;
+              return tmp;
+            }
+          Point* operator*() const
+            {
+              return (*pit).second;
+            }
+          
+        private:
+          typename Points::iterator pit;
+          
+        };
+      
       iterator  begin() { return points.begin(); }
       iterator  end  () { return points.end  (); }
       
+      typename Point::Common* common_data() const { return common; }
+      void set_common_data(typename Point::Common*);
     };
   
 
@@ -182,38 +188,35 @@ namespace GNU_gama {
   template <class Point>
     void PointBase<Point>::put(const Point& point)
     {
-      typename Points::iterator t = points.find(point.name);
-      if (t != points.end())
+      Point* ptr = find(point.name);
+
+      if (ptr)
         {
-          *((*t).second) = point;
+          *ptr = point;
         }
       else
         {
-          Point* ptr  = new Point(point);
-          ptr->common = common;
-          points[point.name] = ptr;
+          ptr = new Point(point);
+          points[ptr->name] = ptr;
         }
-    }
 
+      ptr->common = common;
+    }
 
   template <class Point>
     void PointBase<Point>::put(Point* point_ptr)
     {
-      typename Points::iterator t = points.find(point_ptr->name);
-      if (t != points.end())
+      point_ptr->common = common;
+
+      Point* ptr = find(point_ptr->name);
+      if (ptr)
         {
-          Point* p = (*t).second;
-          if (p != point_ptr)
-            {
-              delete p;
-            }
+          if (ptr == point_ptr) 
+            return;
           else
-            {
-              return;
-            }
+            delete ptr;
         }
 
-      point_ptr->common = common;
       points[point_ptr->name] = point_ptr;
     }
 
@@ -269,6 +272,22 @@ namespace GNU_gama {
 
       points.erase(points.begin(), points.end());
     }  
+
+
+  template <class Point>
+    void PointBase<Point>::set_common_data(typename Point::Common* com)
+    {
+      common = com;
+
+      typename Points::iterator t = points.begin();
+      typename Points::iterator e = points.end();
+      while (t != e)
+        {
+          (*t).second->common = common;
+          ++t;
+        }
+
+    }
 
 }
 
