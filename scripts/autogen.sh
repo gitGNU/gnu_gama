9e887b892628121a30ecacc34e97f80fd54337ec
cepek <cepek>
2006-03-05 11:03:38 +0000

    *** empty log message ***

--- scripts/autogen.sh
+++ scripts/autogen.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-autoreconf --force --install -I config -I m4
+
+# eventually run autoreconf when it will work better
+# autoreconf --force --install -I config
+
+aclocal
+autoheader
+autoconf
+automake -a -c -f
