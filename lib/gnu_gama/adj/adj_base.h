6d9367c1fb65ceff38c5c5247a35536336c6ab65
cepek <cepek>
2006-08-31 12:48:08 +0000

    1.9.01b

--- lib/gnu_gama/adj/adj_base.h
+++ lib/gnu_gama/adj/adj_base.h
@@ -1,8 +1,8 @@
 /*  
-    Geodesy and Mapping C++ Library (GNU GaMa / GaMaLib)
-    Copyright (C) 1999, 2006  Ales Cepek <cepek@gnu.org>
+    GNU Gama -- adjustment of geodetic networks
+    Copyright (C) 2004  Ales Cepek <cepek@gnu.org>
 
-    This file is part of the GNU GaMa / GaMaLib C++ Library.
+    This file is part of the GNU Gama C++ library.
     
     This library is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -20,13 +20,12 @@
 */
 
 /*
- *  $Id: adj_base.h,v 1.11 2006/08/30 18:38:25 cepek Exp $
+ *  $Id: adj_base.h,v 1.12 2006/08/31 12:48:08 cepek Exp $
  */
 
-#ifndef GNU_Gama_gnu_gama_gnugama_GaMa_AdjBaseFull_h
-#define GNU_Gama_gnu_gama_gnugama_GaMa_AdjBaseFull_h
+#ifndef GNU_Gama_gnu_gama_gnugama_GaMa_AdjBase_h
+#define GNU_Gama_gnu_gama_gnugama_GaMa_AdjBase_h
 
-#include <matvec/matvec.h>
 
 namespace GNU_gama {
 
@@ -53,61 +52,6 @@ namespace GNU_gama {
     virtual Float cond() { return Float(); }  // 0 if not available
 
   };
-
-
-
-  template <typename Float, typename Exc>
-  class AdjBaseFull : public AdjBase<Float, Index, Vec<Float, Exc> >
-  {    
-  public:
-
-    AdjBaseFull() : pA(0), pb(0), is_solved(false)
-    {
-    }
-
-    AdjBaseFull(const Mat<Float, Exc>& A, const Vec<Float, Exc>& b)
-      : pA(&A), pb(&b)
-    {
-    }
-
-    virtual ~AdjBaseFull() 
-    {
-    }
-
-    virtual void reset(const Mat<Float, Exc>& A, const Vec<Float, Exc>& b) 
-    {
-      pA = &A;
-      pb = &b;
-      this->is_solved = false;
-    }
-
-    const Vec<Float, Exc>& unknowns() 
-    { 
-      if (!is_solved) solve(); 
-      return x; 
-    }
-
-    const Vec<Float, Exc>& residuals() 
-    { 
-      if (!is_solved) solve(); 
-      return r; 
-    }
-    
-    // solve() must compute vectors x, r  and set is_solved=true
-    virtual void solve() = 0;
-
-
-  protected:
-
-    const Mat<Float, Exc>* pA;
-    const Vec<Float, Exc>* pb;
-
-    Vec<Float, Exc> x;
-    Vec<Float, Exc> r;
-    bool is_solved;
-
-  };
-  
 
 }
 #endif
---
+++ lib/gnu_gama/adj/adj_full.h
@@ -0,0 +1,89 @@
+/*  
+    GNU Gama -- adjustment of geodetic networks
+    Copyright (C) 2006  Ales Cepek <cepek@gnu.org>
+
+    This file is part of the GNU Gama C++ library.
+    
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+ *  $Id: adj_full.h,v 1.1 2006/08/31 12:48:08 cepek Exp $
+ */
+
+#ifndef GNU_Gama_gnu_gama_gnugama_GaMa_AdjBaseFull_h
+#define GNU_Gama_gnu_gama_gnugama_GaMa_AdjBaseFull_h
+
+#include <gnu_gama/adj/adj_base.h>
+
+namespace GNU_gama {
+
+
+  template <typename Float, typename Exc>
+  class AdjBaseFull : public AdjBase<Float, Index, Vec<Float, Exc> >
+  {    
+  public:
+
+    AdjBaseFull() : pA(0), pb(0), is_solved(false)
+    {
+    }
+
+    AdjBaseFull(const Mat<Float, Exc>& A, const Vec<Float, Exc>& b)
+      : pA(&A), pb(&b), is_solved(false)
+    {
+    }
+
+    virtual ~AdjBaseFull() 
+    {
+    }
+
+    virtual void reset(const Mat<Float, Exc>& A, const Vec<Float, Exc>& b) 
+    {
+      pA = &A;
+      pb = &b;
+      is_solved = false;
+    }
+
+    const Vec<Float, Exc>& unknowns() 
+    { 
+      if (!is_solved) solve(); 
+      return x; 
+    }
+
+    const Vec<Float, Exc>& residuals() 
+    { 
+      if (!is_solved) solve(); 
+      return r; 
+    }
+    
+    // solve() must compute vectors x, r  and set is_solved=true
+    virtual void solve() = 0;
+
+
+  protected:
+
+    const Mat<Float, Exc>* pA;
+    const Vec<Float, Exc>* pb;
+
+    Vec<Float, Exc> x;
+    Vec<Float, Exc> r;
+    bool is_solved;
+
+  };
+  
+
+}
+#endif
+
