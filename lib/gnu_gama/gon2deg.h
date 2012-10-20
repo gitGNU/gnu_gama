e3d69060ee1df1e653e0358cc16e6c19197d761e
Ales Cepek <cepek@gnu.org>
2012-10-20 22:44:04 +0200

    added conditional definition of macro M_PI

--- lib/gnu_gama/gon2deg.h
+++ lib/gnu_gama/gon2deg.h
@@ -1,29 +1,32 @@
-/*
-    GNU Gama -- adjustment of geodetic networks
-    Copyright (C) 2004, 2010  Ales Cepek <cepek@gnu.org>
+/* GNU Gama -- adjustment of geodetic networks
+   Copyright (C) 2004, 2012, 2012  Ales Cepek <cepek@gnu.org>
 
-    This file is part of the GNU Gama C++ Library.
+   This file is part of the GNU Gama C++ library.
 
-    This library is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
 
 #include <string>
 
 #ifndef GNU_gama_gons_to_degrees_h___GNU_Gama_gon2deg__gon2deg
 #define GNU_gama_gons_to_degrees_h___GNU_Gama_gon2deg__gon2deg
 
+#ifndef M_PI
+//           3.141592653589793238462643383279502884197169399375105820...
+#define M_PI 3.14159265358979323846264338327950288419716939937510
+#endif
 
 namespace GNU_gama {
 
@@ -39,13 +42,4 @@ namespace GNU_gama {
   double rad2dms(double);
 }
 
-
 #endif
-
-
-
-
-
-
-
-
