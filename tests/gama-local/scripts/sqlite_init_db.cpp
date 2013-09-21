f9a4bd670b194546430d1f3c33fd8fb6a75da866
Ales Cepek <cepek@gnu.org>
2013-09-21 23:09:42 +0200

    a better implementation of sqlite3 DB create and data population

--- tests/gama-local/scripts/sqlite_init_db.cpp
+++ tests/gama-local/scripts/sqlite_init_db.cpp
@@ -3,93 +3,81 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cctype>
+#include <iterator>
 
-sqlite3    *dbHandle;
-const char *pzTail;
+sqlite3    *database;
 int         error = 0;
 
-void read_sql_file(const char*);
+int read_sql_file(const char*);
 
 int main(int argc, const char* argv[])
 {
   std::string dbname = argv[1];
-
-  int resOpen = sqlite3_open_v2(dbname.c_str(), &dbHandle,
-                                SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE,
-                                0);
-  if (resOpen != SQLITE_OK)
+  int open = sqlite3_open_v2(dbname.c_str(), &database,
+                             SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, 0);
+  if (open != SQLITE_OK)
     {
-      std::cout << "DB open error " << resOpen << "\n";
+      std::cout << "DB open error " << open << "\n";
       return 1;
     }
 
   for (int i=2; i<argc && !error; i++) read_sql_file(argv[i]);
 
-  int resClose = sqlite3_close/*_v2*/(dbHandle);
-  if (resClose != SQLITE_OK)
+  int close = sqlite3_close/*_v2*/(database);
+  if (close != SQLITE_OK)
     {
-      std::cout << "DB close error " << resClose << "\n";
+      std::cout << "DB close error " << close << "\n";
       return 1;
     }
 
   return error;
 }
 
-void read_sql_file(const char* file_name)
+int read_sql_file(const char* file_name)
 {
-  //std::cout << "reading file " << file_name << "\n";
-  std::ifstream file(file_name);
+  std::ifstream finp(file_name);
+  if(!finp) return error=1;
 
-  while(file)
+  std::string sinp;
+
+  finp.seekg(0, std::ios::end);
+  sinp.resize(finp.tellg());
+  finp.seekg(0, std::ios::beg);
+
+  finp.unsetf(std::ios_base::skipws);
+  sinp.assign(std::istream_iterator<char>(finp),
+              std::istream_iterator<char>());
+
+  const char* tail = 0;
+  const char* sql  = sinp.c_str();
+  const char* stop = sql + sinp.length();
+
+  do
     {
-      char c;
-      bool comment = false, empty = true;;
-      std::string statement;
-      while (file.get(c))
-        {
-          if (c == '/' && file.peek() == '*') {
-            comment = true;  file.get(c); continue;
-          }
-          if (c == '*' && file.peek() == '/') {
-            comment = false; file.get(c); continue;
-          }
-          if (!comment && c == ';') break;
+      sqlite3_stmt *statement;
+      int prepare = sqlite3_prepare_v2(database, sql, -1, &statement, &tail);
+      if (prepare != SQLITE_OK) {
+        std::cout << "DB prepare error " << prepare << "\n";
+        return error=1;
+      }
 
-          if (!comment) {
-            statement += c;
-            if (!std::isspace(c)) empty = false;
-          }
-        }
-      if (empty) continue;
+      int step;
+      for (;;) {
+        step = sqlite3_step(statement);
+        if (step != SQLITE_ROW) break;
 
-      sqlite3_stmt *ppStmt;
-      int resPrepare = sqlite3_prepare_v2(
-             dbHandle,           /* Database handle */
-             statement.c_str(),  /* SQL statement, UTF-8 encoded */
-             statement.length(), /* Maximum length of zSql in bytes. */
-             &ppStmt,            /* OUT: Statement handle */
-             &pzTail);           /* OUT: Pointer to unused portion of zSql */
-      if (resPrepare != SQLITE_OK)
-        {
-          std::cout << "DB prepare error " << resPrepare << "\n";
-          error = 1;
-          return;
-        }
+        // select processing ... sqlite3_column_int(statement, 0)
+      }
 
-      int resStep;
-      do  
-        {
-          resStep = sqlite3_step(ppStmt);
-        }
-      while (resStep == SQLITE_ROW);
+      int final = sqlite3_finalize(statement);
+      if (final != SQLITE_OK) {
+        std::cout << "DB finalize error " << final << "\n";
+        return error=1;
+      }
 
-      int resFinal = sqlite3_finalize(ppStmt);
-      if (resFinal != SQLITE_OK)
-        {
-          std::cout << "DB finalize error " << resFinal << "\n";
-          error = 1;
-          return;
-        }
+      sql = tail;
     }
+  while (tail && tail < stop);
+
+  return 0;
 }
