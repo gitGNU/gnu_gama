2e3d677d6355d7a9355431bd5ff1c208aa71367f
Ales Cepek <cepek@gnu.org>
2013-10-12 23:03:51 +0200

    Adjustment result data moved from LocalNetworkAdjustmentResults to LocalNetworkAdjustmentResultsData

---
+++ lib/gnu_gama/xml/localnetwork_adjustment_results_data.cpp
@@ -0,0 +1,96 @@
+/* GNU Gama -- adjustment of geodetic networks
+   Copyright (C) 2013  Ales Cepek <cepek@gnu.org>
+
+   This file is part of the GNU Gama C++ library.
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+/** \file localnetwork_adjustmet_results_data.cpp
+ * \brief #GNU_gama::local::LocalNetworkAdjustmentResultsData class implementation
+ *
+ * \author Ales Cepek
+ */
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <gnu_gama/xml/localnetwork_adjustment_results_data.h>
+#include <gnu_gama/intfloat.h>
+#include <gnu_gama/xml/htmlparser.h>
+
+using namespace std;
+using namespace GNU_gama;
+
+LocalNetworkAdjustmentResultsData::LocalNetworkAdjustmentResultsData()
+{
+  init();
+}
+
+void LocalNetworkAdjustmentResultsData::init()
+{
+  gons = true;
+
+  description.clear();
+
+  network_general_parameters.gama_local_version.  clear();
+  network_general_parameters.gama_local_algorithm.clear();
+  network_general_parameters.gama_local_compiler. clear();
+  network_general_parameters.epoch.  clear();
+  network_general_parameters.axes_xy.clear();
+  network_general_parameters.angles. clear();
+
+  coordinates_summary.adjusted.xyz = 0;
+  coordinates_summary.adjusted.xy  = 0;
+  coordinates_summary.adjusted.z   = 0;
+  coordinates_summary.constrained.xyz = 0;
+  coordinates_summary.constrained.xy  = 0;
+  coordinates_summary.constrained.z   = 0;
+  coordinates_summary.fixed.xyz = 0;
+  coordinates_summary.fixed.xy  = 0;
+  coordinates_summary.fixed.z   = 0;
+
+  observations_summary.distances = 0;
+  observations_summary.directions = 0;
+  observations_summary.angles = 0;
+  observations_summary.xyz_coords = 0;
+  observations_summary.h_diffs = 0;
+  observations_summary.z_angles = 0;
+  observations_summary.s_dists = 0;
+  observations_summary.vectors = 0;
+  observations_summary.azimuths = 0;
+
+  project_equations.equations = 0;
+  project_equations.unknowns = 0;
+  project_equations.degrees_of_freedom = 0;
+  project_equations.defect = 0;
+  project_equations.sum_of_squares = 0;
+  project_equations.connected_network = true;
+
+  standard_deviation.apriori = 0;
+  standard_deviation.aposteriori = 0;
+  standard_deviation.using_aposteriori = true;
+  standard_deviation.probability = 0;
+  standard_deviation.ratio = 0;
+  standard_deviation.lower = 0;
+  standard_deviation.upper = 0;
+  standard_deviation.passed = false;
+  standard_deviation.confidence_scale = 0;
+
+  fixed_points      .clear();
+  approximate_points.clear();
+  adjusted_points   .clear();
+}
---
+++ lib/gnu_gama/xml/localnetwork_adjustment_results_data.h
@@ -0,0 +1,186 @@
+/* GNU Gama -- adjustment of geodetic networks
+   Copyright (C) 2013  Ales Cepek <cepek@gnu.org>
+
+   This file is part of the GNU Gama C++ library.
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+/** \file localnetwork_adjustmet_results_data.h
+ * \brief #GNU_gama::local::LocalNetworkAdjustmentResultsData class implementation
+ *
+ * \author Ales Cepek
+ */
+
+#ifndef GNU_gama_localnetwork_adjustment_results_data__DATA_h
+#define GNU_gama_localnetwork_adjustment_results_data__DATA_h
+
+#include <string>
+#include <vector>
+#include <matvec/covmat.h>
+
+
+namespace GNU_gama
+{
+  class LocalNetworkAdjustmentResultsData
+  {
+  public:
+
+    LocalNetworkAdjustmentResultsData();
+
+    bool gons;
+    std::string description;
+
+    struct
+    {
+      std::string gama_local_version;
+      std::string gama_local_algorithm;
+      std::string gama_local_compiler;
+      std::string epoch;
+      std::string axes_xy;
+      std::string angles;
+
+    } network_general_parameters;
+
+    struct count
+    {
+      int xyz, xy, z;
+    };
+
+    struct
+    {
+      count adjusted;
+      count constrained;
+      count fixed;
+
+    } coordinates_summary;
+
+    struct
+    {
+      int distances;
+      int directions;
+      int angles;
+      int xyz_coords;
+      int h_diffs;
+      int z_angles;
+      int s_dists;
+      int vectors;
+      int azimuths;
+
+    } observations_summary;
+
+    struct
+    {
+      int    equations;
+      int    unknowns;
+      int    degrees_of_freedom;
+      int    defect;
+      double sum_of_squares;
+      bool   connected_network;
+
+    } project_equations;
+
+    struct
+    {
+      double apriori;
+      double aposteriori;
+      bool   using_aposteriori;
+      double probability;
+      double ratio;
+      double lower;
+      double upper;
+      bool   passed;
+      double confidence_scale;
+
+    } standard_deviation;
+
+    struct Point
+    {
+      std::string id;
+      double x, y, z;
+
+      bool   hxy, hz;             // point has   x, y, z
+      bool   cxy, cz;             // constrained x, y, z
+      int    indx, indy, indz;    // adjustment indexes
+
+      void clear()
+      {
+        x=y=z=0;
+        hxy=hz=cxy=cz=false;
+        indx=indy=indz=0;
+      }
+    };
+
+    typedef std::vector<Point> PointList;
+    PointList  fixed_points, approximate_points, adjusted_points;
+
+    struct Orientation
+    {
+      std::string id;
+      double      approx;
+      double      adj;
+      int         index;          // adjustment index
+    };
+
+    typedef std::vector<Orientation> OrientationList;
+    OrientationList orientations;
+
+    CovMat<> cov;
+
+    std::vector<int> original_index; // original indexes from the adjustment
+
+    struct Observation
+    {
+      std::string xml_tag;
+      std::string from;
+      std::string to;
+      std::string left;         // used in angle observation
+      std::string right;        //  ....   angle   ....
+
+      double obs;               // observed value
+      double adj;               // adjusted
+      double stdev;             // standard deviation of adj. value
+      double qrr;               // weight coefficient of the residual
+      double f;
+
+      double      std_residual; // standardized residual
+      std::string err_obs;      // estimate of observed value error
+      std::string err_adj;      //  ....       adjusted  ....
+
+      void clear()
+      {
+        obs = adj = stdev = qrr = f = std_residual = 0;
+        xml_tag     .clear();
+        from        .clear();
+        to          .clear();
+        left        .clear();
+        right       .clear();
+        err_obs     .clear();
+        err_adj     .clear();
+      }
+
+      double residual() const throw();
+    };
+
+    typedef std::vector<Observation> ObservationList;
+    ObservationList  obslist;
+
+  private:
+
+    void init();
+
+  };
+}
+
+#endif
