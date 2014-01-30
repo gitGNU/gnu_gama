19fc735b0d06c82371bb59877b750e41a6274ba7
Ales Cepek <cepek@gnu.org>
2014-01-30 21:31:30 +0100

    added ../lib/gnu_gama/xsd.h (XML Schema nanespaces)

---
+++ lib/gnu_gama/xsd.h
@@ -0,0 +1,32 @@
+/* GNU Gama -- adjustment of geodetic networks
+   Copyright (C) 2014  Ales Cepek <cepek@gnu.org>
+
+   This file is part of the GNU Gama C++ library.
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+/** \file xsd.h
+ * \brief GNU_gama XML Schema namespaces
+ * 
+ * \author Ales Cepek
+ */
+
+#ifndef XSD_GAMA_LOCAL
+#define XSD_GAMA_LOCAL "http://www.gnu.org/software/gama/gama-local"
+#endif
+
+#ifndef XSD_GAMA_LOCAL_ADJUSTMENT
+#define XSD_GAMA_LOCAL_ADJUSTMENT "http://www.gnu.org/software/gama/gama-local-adjustment"
+#endif
